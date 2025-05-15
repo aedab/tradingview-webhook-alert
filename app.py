e1b949e185d66fd2a1765017f818f6941314e60a
aedab <andrei.vanzari@gmail.com>
2025-05-15 11:52:49 +0200

    add port

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import requests
 import os
 
 app = Flask(__name__)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 locally
+    app.run(host="0.0.0.0", port=port)
 
 # Telegram setup
 TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
